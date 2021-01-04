7b15e2489c4348a2a54de9ecf7bd66651c928c2e
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-01-05 00:29:20 +0800

    Created Nav Bar

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('#right-nav')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
