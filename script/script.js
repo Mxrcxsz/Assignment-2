8239cd8bf08132773bb2478a96b5fb178d02cbed
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-01-05 20:19:23 +0800

    Added Main Content

--- script/script.js
+++ script/script.js
@@ -6,3 +6,12 @@ menu.addEventListener('click', function(){
     menuLinks.classList.toggle('active');
 })
 
+fetch('https://mxrcxsz.github.io/asg2/db.json')
+    .then(response => response.json()) 
+    .then(function(data){
+        for(var i = 0; i <= data.count; i++)
+        {
+            console.log(data[i]);
+            console.log('hi');
+        }
+    });    
