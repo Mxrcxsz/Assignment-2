91a17a8523975b91220a725387c05b47cab9371c
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-01-06 00:28:51 +0800

    Testing for API

--- script/script.js
+++ script/script.js
@@ -4,14 +4,47 @@ const menuLinks = document.querySelector('#right-nav')
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+    if(menuLinks.className == "active"){
+        $('header').css("box-shadow","none");
+    }
+    else{
+        $('header').css("box-shadow","2px 1px 9px rgb(10, 16, 26)");
+        $('header').css("transition","all 0.5s ease");
+    }
 })
 
-fetch('https://mxrcxsz.github.io/asg2/db.json')
-    .then(response => response.json()) 
-    .then(function(data){
-        for(var i = 0; i <= data.count; i++)
-        {
-            console.log(data[i]);
-            console.log('hi');
+// All Map API
+// Class for hdb carpark details
+function get_carpark_details(){
+    $.ajax({
+        url: 'https://api.jsonbin.io/b/5ff45d9709f7c73f1b6df03d',
+        type: "GET", //send it through GET method
+        headers: {
+            "content-type":"application/json",
+            "secret-key":"$2b$10$U32F5.Qe7ErRiSaybhPmd.XQ8oikTg4jGxtm0x3zN23lMpohizeva"
+        },
+        success: function(data) {
+            console.log(data);
         }
-    });    
+    });
+}
+
+function get_carpark_nearby(lat, long, radius){
+    $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+        type: "GET", //send it through GET method
+        headers: {
+            "location":lat + "," + long,
+            "radius":radius,
+            "types":"parking",
+            "key":"AIzaSyAaDnggQoyZ9Rv8U6nwIq-iQ0gNtSswlzg",
+            "Access-Control-Allow-Origin": "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+            "Access-Control-Allow-Origin": "true"
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
+
+get_carpark_nearby(1.2984498,103.7988242,500);
